a1830547047c223c1f6f75002f8110809657a23d
DarioGonzalez-Axians <dario.gonzalez@axians.es>
2020-02-27 03:41:44 +0100

    Style - title, footer...

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'ngx-footer',
-  styleUrls: ['./footer.component.scss'],
-  template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2019</span>
+    selector: 'ngx-footer',
+    styleUrls: ['./footer.component.scss'],
+    template: `
+    <span class="created-by">By Dario Gonzalez for Telxius<b> 27-02-2020 </b> (Based on ngx-admin by Arkveo)</span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="https://github.com/dglezgall" target="_blank" class="ion ion-social-github"></a>
+      <a href="https://linkedin.com/in/d-glez-gallego" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
 })

83c8ca26406566e90cb328182902c796b09e6a9c
DarioGonzalez-Axians <dario.gonzalez@axians.es>
2020-02-27 18:01:39 +0100

    Feature - gmaps markers

--- src/app/pages/maps/gmaps/gmaps.component.ts
+++ src/app/pages/maps/gmaps/gmaps.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { JsonUser } from '../../../models/JsonUser';
+import { Location } from '../../../models/Location';
+import { Marker } from '../../../models/Marker';
 import { JsonUserService } from '../../../services/users.service';
 
 @Component({
@@ -10,15 +12,25 @@ import { JsonUserService } from '../../../services/users.service';
 })
 export class GmapsComponent implements OnInit {
 
-    lat = 51.678418;
-    lng = 7.809007;
+    location: Location;
+    selectedMarker: Marker;
 
     users: JsonUser[] = [];
+    user: JsonUser;
 
     constructor(public jsonUserService: JsonUserService) {
     }
 
     ngOnInit(): void {
+
+        this.location = {
+            latitude: 10,
+            longitude: 10,
+            mapType: "satelite",
+            zoom: 5,
+            markers: [],
+        }
+
         this.jsonUserService.getUsers()
             .subscribe(
                 users => {
@@ -26,12 +38,35 @@ export class GmapsComponent implements OnInit {
                 },
                 err => console.error(err),
             );
-        while (true) {
-            break;
+
+    }
+
+    addMarker(lat: number, lng: number) {
+        this.location.markers.push({
+            lat,
+            lng
+        })
+    }
+
+    selectMarker(event: any) {
+        this.selectedMarker = {
+            lat: event.latitude,
+            lng: event.longitude
         }
+    }
+
+    markerDragEnd(coords: any) {
+        this.location.latitude = coords.latitude
+        this.location.longitude = coords.longitude
     }
 
     randomInt(min: number, max: number) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    addUsersMarker() {
+        const user = this.users[this.randomInt(0, 10)];
+        this.addMarker(+user.address.geo.lat, +user.address.geo.lng);
+    }
 }
+
--- src/app/pages/maps/maps.module.ts
+++ src/app/pages/maps/maps.module.ts
@@ -1,27 +1,30 @@
 import { NgModule } from '@angular/core';
 import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { NgxEchartsModule } from 'ngx-echarts';
-import { NbCardModule } from '@nebular/theme';
+import { NbCardModule, NbButtonModule } from '@nebular/theme';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { MapsRoutingModule, routedComponents } from './maps-routing.module';
 
 @NgModule({
-  imports: [
-    ThemeModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCpVhQiwAllg1RAFaxMWSpQruuGARy0Y1k',
-      libraries: ['places'],
-    }),
-    LeafletModule.forRoot(),
-    MapsRoutingModule,
-    NgxEchartsModule,
-    NbCardModule,
-  ],
-  exports: [],
-  declarations: [
-    ...routedComponents,
-  ],
+    imports: [
+        ThemeModule,
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyCpVhQiwAllg1RAFaxMWSpQruuGARy0Y1k',
+            libraries: ['places'],
+        }),
+        LeafletModule.forRoot(),
+        MapsRoutingModule,
+        NgxEchartsModule,
+        NbCardModule,
+        AgmDirectionModule,
+        NbButtonModule
+    ],
+    exports: [],
+    declarations: [
+        ...routedComponents,
+    ],
 })
 export class MapsModule { }
